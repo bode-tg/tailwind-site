8a07b8e31b82db63c21212c8a750c6f6c60eed48
Olabode <bode.ladejobi@gmail.com>
2022-09-20 22:52:12 +0100

    hero section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html',],
+  content: ['./*.html',],
   theme: {
-    screens: {},
-    extend: {},
+    screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px'},
+    extend: {
+      textcolor: theme => theme('colors'),
+      textColor: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+      scale: ['active'],
+      backgroundColor: ['active'],
+    }
   },
+
   plugins: [],
 }
